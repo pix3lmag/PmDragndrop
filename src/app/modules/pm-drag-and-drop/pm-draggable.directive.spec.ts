6d61455ddeab227a54b0a1bf6230452af360690d
Brotrueck, Michael <mbrotrueck@avp.de>
2019-11-08 11:57:34 +0100

    initial commit

---
+++ src/app/modules/pm-drag-and-drop/pm-draggable.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PmDraggableDirective } from './pm-draggable.directive';
+
+describe('PmDraggableDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PmDraggableDirective();
+    expect(directive).toBeTruthy();
+  });
+});
